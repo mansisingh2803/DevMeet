c6a4c5332c7d88e501fec680616f419d12898652
Mansi Singh <mansisinghsengar28@gmail.com>
2026-01-05 03:09:28 +0530

    app changes

--- src/app.js
+++ src/app.js
@@ -17,10 +17,10 @@ const requestRouter = require("./routes/requests");
 const userRouter = require('./routes/user');
 
 
-app.use("/", authRouter);
-app.use("/", profileRouter);
-app.use("/", requestRouter);
-app.use("/", userRouter);
+app.use("/api", authRouter);
+app.use("/api", profileRouter);
+app.use("/api", requestRouter);
+app.use("/api", userRouter);
 
 
 //proper way to connecting the database 
